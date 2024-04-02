0be8650d84a23c60d881e42792fd2744ed201860
5OO <git.hub@myrk.eu>
2024-04-02 19:10:51 +0300

    Improve styling and structure in FiltersList
    Refactored the FiltersList component to improve readability and user interaction. Specifically, implemented Bootstrap classes to enhance element structure and button styling. Now, each filter is listed as a proper list item, with a visibly appealing "Apply" button.

--- src/components/FiltersList.vue
+++ src/components/FiltersList.vue
@@ -18,23 +18,23 @@ export default {
         })
         .catch(error => {
           console.error('There was an error fetching the filters: ', error)
-        });
+        })
     },
     applyFilter(filterId) {
       // Placeholder for applying a filter
-      console.log("Applying filter with ID: ", filterId);
-    },
+      console.log('Applying filter with ID: ', filterId)
+    }
   }
 }
 </script>
 
 <template>
-  <div>
-    <h2>Available Filters</h2>
-    <ul>
-      <li v-for="filter in filters" :key="filter.id">
+  <div class="container">
+    <h2 class="mt-4">Available Filters</h2>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="filter in filters" :key="filter.id">
         {{ filter.name }}
-        <button @click="applyFilter(filter.id)">Apply</button>
+        <button class="btn btn-primary" @click="applyFilter(filter.id)">Apply</button>
       </li>
     </ul>
   </div>

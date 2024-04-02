00ea9b23ae83bb203f087445b152c0b0dfbccf06
5OO <git.hub@myrk.eu>
2024-04-02 18:53:59 +0300

    Add FiltersList component with API fetch
    Added a new FiltersList component in Vue.js for fetching filter data from the API. In addition, error handling is incorporated for any issues that might arise during this process.

---
+++ src/components/FiltersList.vue
@@ -0,0 +1,37 @@
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      filters: []
+    }
+  },
+  created() {
+    this.fetchFilters()
+  },
+  methods: {
+    fetchFilters() {
+      axios.get('/api/filters')
+        .then(response => {
+          this.filters = response.data
+        })
+        .catch(error => {
+          console.error('There was an error fetching the filters: ', error)
+        })
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+.filters-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>

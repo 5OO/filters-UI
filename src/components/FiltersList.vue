4b4a5ccbcfe3cfe2c737bec779890fa5316d54ec
5OO <git.hub@myrk.eu>
2024-04-02 19:09:45 +0300

    Add applyFilter method and update template in FiltersList
    Added an applyFilter method in FiltersList component to handle filter application. Also, updated the template structure to include filter information and an apply button for each filter.

--- src/components/FiltersList.vue
+++ src/components/FiltersList.vue
@@ -18,20 +18,24 @@ export default {
         })
         .catch(error => {
           console.error('There was an error fetching the filters: ', error)
-        })
-    }
+        });
+    },
+    applyFilter(filterId) {
+      // Placeholder for applying a filter
+      console.log("Applying filter with ID: ", filterId);
+    },
   }
 }
 </script>
 
 <template>
-
+  <div>
+    <h2>Available Filters</h2>
+    <ul>
+      <li v-for="filter in filters" :key="filter.id">
+        {{ filter.name }}
+        <button @click="applyFilter(filter.id)">Apply</button>
+      </li>
+    </ul>
+  </div>
 </template>
-
-<style scoped>
-.filters-container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-}
-</style>

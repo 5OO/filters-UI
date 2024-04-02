34dbb4b6d454081eb216b7139752edc8b9db09d9
5OO <git.hub@myrk.eu>
2024-04-02 22:57:40 +0300

    Add PrimeVue dependencies and setup in main.js
    This commit adds the PrimeVue, PrimeFlex and PrimeIcons dependencies into package.json and sets up PrimeVue in main.js. It also includes adding the necessary PrimeVue, PrimeIcons and PrimeFlex styles in main.js.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import 'primevue/resources/themes/saga-blue/theme.css' // theme
+import 'primevue/resources/primevue.min.css' // core css
+import 'primeicons/primeicons.css' // icons
+import 'primeflex/primeflex.css';
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.mount('#app');

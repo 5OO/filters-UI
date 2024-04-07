b31167575dd1b4d50a35f0b50c8faee1e6095125
5OO <git.hub@myrk.eu>
2024-04-07 16:24:53 +0300

    fix input validation in FilterModal component
    DatePicker does not need to deliver timestamp, fix implements date only format.

--- src/components/FilterModal.vue
+++ src/components/FilterModal.vue
@@ -1,26 +1,26 @@
 <script setup>
 
 import { ref, watch } from 'vue'
-import axios from 'axios';
-import PvDialog from 'primevue/dialog';
-import PvButton from 'primevue/button';
-import PvInputText from 'primevue/inputtext';
-import { eventBus } from '@/eventBus.js';
-import PvDropdown from 'primevue/dropdown';
-import PvCalendar from 'primevue/calendar';
-import  PvInputNumber from 'primevue/inputnumber';
+import axios from 'axios'
+import PvDialog from 'primevue/dialog'
+import PvButton from 'primevue/button'
+import PvInputText from 'primevue/inputtext'
+import { eventBus } from '@/eventBus.js'
+import PvDropdown from 'primevue/dropdown'
+import PvCalendar from 'primevue/calendar'
+import PvInputNumber from 'primevue/inputnumber'
 
-const showDialog = ref(false);
-const filterName = ref('');
+const showDialog = ref(false)
+const filterName = ref('')
 const criteria = ref([
-  {fieldName:'',comparisonOperator:'', criteriaValue:'', comparisonOptions: [], fieldType:''}
-]);
+  { fieldName: '', comparisonOperator: '', criteriaValue: '', comparisonOptions: [], fieldType: '' }
+])
 const addCriteria = () => {
-  criteria.value.push({fieldName:'',comparisonOperator:'', criteriaValue:'', comparisonOptions: []});
+  criteria.value.push({ fieldName: '', comparisonOperator: '', criteriaValue: '', comparisonOptions: [] })
 }
 
 const removeCriteria = (index) => {
-  criteria.value.splice(index, 1);
+  criteria.value.splice(index, 1)
 }
 
 const fieldOptions = ref([ // Define your field options
@@ -29,61 +29,75 @@ const fieldOptions = ref([ // Define your field options
   { label: 'Release Date', value: 'releaseDate' },
   { label: 'Vote Average', value: 'voteAverage' },
   { label: 'Popularity', value: 'popularity' }
-]);
+])
 
 const comparisonOptionsByType = ref({
   string: [
     { label: 'Contains', value: 'contains' },
     { label: 'Starts With', value: 'startsWith' },
     { label: 'Ends With', value: 'endsWith' },
-    { label: 'Equals', value: 'equals' },
+    { label: 'Equals', value: 'equals' }
   ],
   numeric: [
     { label: 'Greater Than', value: '>' },
     { label: 'Less Than', value: '<' },
-    { label: 'Equals', value: '=' },
+    { label: 'Equals', value: '=' }
   ],
   date: [
     { label: 'After', value: 'after' },
     { label: 'Before', value: 'before' },
     { label: 'On', value: 'equals' },
     { label: 'After or On', value: '>=' },
-    { label: 'Before or On', value: '<=' },
-  ],
-});
+    { label: 'Before or On', value: '<=' }
+  ]
+})
 
 const updateComparisonOptions = (criterion) => {
-  let type;
+  let type
   switch (criterion.fieldName) {
     case 'voteAverage':
     case 'popularity':
-      type = 'numeric';
-      break;
+      type = 'numeric'
+      break
     case 'originalTitle':
     case 'title':
-      type = 'string';
-      break;
+      type = 'string'
+      break
     case 'releaseDate':
-      type = 'date';
-      break;
+      type = 'date'
+      break
     default:
-      type = 'string'; // Default case
+      type = 'string' // Default case
   }
-  criterion.fieldType = type;
-  criterion.comparisonOptions = comparisonOptionsByType.value[type];
-};
+  criterion.fieldType = type
+  criterion.comparisonOptions = comparisonOptionsByType.value[type]
+}
 
 const saveFilter = async () => {
   try {
+    const formattedCriteria = criteria.value.map(criterion => {
+      if (criterion.fieldType === 'date' && criterion.criteriaValue) {
+        const date = new Date(criterion.criteriaValue)
+        const formattedDate = date.toISOString().split('T')[0]
+        return { ...criterion, criteriaValue: formattedDate }
+      }
+      return criterion
+    })
     const response = await axios.post('http://localhost:8080/api/filters', {
       name: filterName.value,
-      criteria: criteria.value
+      criteria: formattedCriteria
     })
     console.log('Filter saved:', response.data)
     filterName.value = ''
-    criteria.value = [{fieldName:'',comparisonOperator:'', criteriaValue:'',  comparisonOptions: []}]
+    criteria.value = [{
+      fieldName: '',
+      comparisonOperator: '',
+      criteriaValue: '',
+      comparisonOptions: [],
+      fieldType: ''
+    }]
     showDialog.value = false
-    eventBus.emit('filter-saved', { message: '... refreshing list ...' });
+    eventBus.emit('filter-saved', { message: '... refreshing list ...' })
   } catch (error) {
     console.error('Error saving filter:', error)
   }
@@ -91,43 +105,47 @@ const saveFilter = async () => {
 
 criteria.value.forEach((criterion) => {
   watch(() => criterion.fieldName, () => {
-    updateComparisonOptions(criterion);
-  }, { immediate: true });
-});
+    updateComparisonOptions(criterion)
+  }, { immediate: true })
+})
 
 </script>
 
-  <template>
-    <PvButton label="Create New Filter" @click="showDialog = true" class="create-filter-button" />
-    <PvDialog v-model:visible="showDialog" :modal="true" :closable="false" :style="{ width: '60rem' }">
-      <template #header>
-        <h3>Create New Filter</h3>
-      </template>
-        <div class="p-fluid">
-          <div class="p-field filter-name">
-            <label for="filterName">Filter Name</label>
-            <PvInputText id="filterName" v-model="filterName" placeholder="Enter filter name"/>
-          </div>
-          <!-- Criteria Rows -->
-          <div v-for="(criterion, index) in criteria" :key="index" class="p-field criteria-row">
-            <PvDropdown v-model="criterion.fieldName" :options="fieldOptions" placeholder="Select Field Name" optionLabel="label" optionValue="value" />
-            <PvDropdown v-model="criterion.comparisonOperator" :options="criterion.comparisonOptions" placeholder="Select Comparison Operator" optionLabel="label" optionValue="value"/>
+<template>
+  <PvButton label="Create New Filter" @click="showDialog = true" class="create-filter-button" />
+  <PvDialog v-model:visible="showDialog" :modal="true" :closable="false" :style="{ width: '60rem' }">
+    <template #header>
+      <h3>Create New Filter</h3>
+    </template>
+    <div class="p-fluid">
+      <div class="p-field filter-name">
+        <label for="filterName">Filter Name</label>
+        <PvInputText id="filterName" v-model="filterName" placeholder="Enter filter name" />
+      </div>
+      <!-- Criteria Rows -->
+      <div v-for="(criterion, index) in criteria" :key="index" class="p-field criteria-row">
+        <PvDropdown v-model="criterion.fieldName" :options="fieldOptions" placeholder="Select Field Name"
+                    optionLabel="label" optionValue="value" />
+        <PvDropdown v-model="criterion.comparisonOperator" :options="criterion.comparisonOptions"
+                    placeholder="Select Comparison Operator" optionLabel="label" optionValue="value" />
 
-            <PvCalendar v-if="criterion.fieldType === 'date'" v-model="criterion.criteriaValue" placeholder="Select a date" />
-            <PvInputNumber v-else-if="criterion.fieldType === 'numeric'" v-model="criterion.criteriaValue" placeholder="Enter a number" />
-            <PvInputText v-else v-model="criterion.criteriaValue" placeholder="Enter text" />
+        <PvCalendar v-if="criterion.fieldType === 'date'" v-model="criterion.criteriaValue"
+                    placeholder="Select a date" />
+        <PvInputNumber v-else-if="criterion.fieldType === 'numeric'" v-model="criterion.criteriaValue"
+                       placeholder="Enter a number" />
+        <PvInputText v-else v-model="criterion.criteriaValue" placeholder="Enter text" />
 
-            <PvButton label="remove row" @click="removeCriteria(index)" />
-          </div>
-          <PvButton label="Add Criteria" @click="addCriteria" />
-        </div>
+        <PvButton label="remove row" @click="removeCriteria(index)" />
+      </div>
+      <PvButton label="Add Criteria" @click="addCriteria" />
+    </div>
 
-      <template #footer>
-        <PvButton label="Save" @click="saveFilter" />
-        <PvButton label="Cancel" @click="showDialog = false" />
-      </template>
-    </PvDialog>
-  </template>
+    <template #footer>
+      <PvButton label="Save" @click="saveFilter" />
+      <PvButton label="Cancel" @click="showDialog = false" />
+    </template>
+  </PvDialog>
+</template>
 
 <style scoped>
 .create-filter-button {
@@ -141,6 +159,7 @@ criteria.value.forEach((criterion) => {
   align-items: center;
   padding: 5px;
 }
+
 .criteria-row {
   display: flex;
   gap: 5px;
